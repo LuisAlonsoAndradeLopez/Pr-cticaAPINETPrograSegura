85c86b97cca9faed74f2d4be505829b75dfdf355
LuisAlonsoAndradeLopez <caffeinated340@gmail.com>
2024-05-17 00:57:26 -0600

    Adding Files

---
+++ bd.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS netflixnet;
+CREATE DATABASE netflixnet CHARACTER SET utf8mb4;
+USE netflixnet;
+
+/*
+CREATE USER netflix_user@localhost IDENTIFIED BY 'N3tf1x';
+GRANT ALL ON netflixnet.* TO netflix_user@localhost;
+GRANT SELECT, INSERT, UPDATE, DELETE ON netflixnet.* TO netflix_user@localhost;
+REVOKE ALL ON netflixnet.* FROM netflix_user@localhost;
+
+SHOW GRANTS FOR netflix_user@localhost;
+*/
